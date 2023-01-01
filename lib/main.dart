6b9f6930b439c1e7f42545024c42ddf24799e9e3
Muhammed <muhammednihalit@gmail.com>
2023-01-01 13:17:27 +0530

    listview using divider widget

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+void main(List<String> args) {
+  runApp(
+      myapp()); //function to run the app,passing myapp-(myapp class runs when app runs)
+}
+
+class myapp extends StatelessWidget {
+  const myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: homescreen(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
+
+class homescreen extends StatelessWidget {
+  const homescreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white, //red bg
+        appBar: AppBar(
+          title: Text("creating a list"),
+        ),
+        body: SafeArea(
+            child: ListView.separated(
+                itemBuilder: (ctx, index) {
+                  return Text('text $index');
+                },
+                separatorBuilder: (ctx, index) {
+                  return Divider();
+                },
+                itemCount: 50)));
+  }
+}
